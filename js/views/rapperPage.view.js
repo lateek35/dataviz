8d8dce15ddcfbcf4de9e8135a40fd055e1a60d5d
kevinchap <kevin.chapron@hetic.net>
2014-06-13 07:28:11 +0200

    Add module fiche rapper

--- js/views/rapperPage.view.js
+++ js/views/rapperPage.view.js
@@ -1,4 +1,4 @@
-define(['backbone','modelRapper','collectionRapper','text!templates/page-rappeur.html'], function (backbone, modelRapper,collectionRapper,jstemplate) {
+define(['backbone','modelRapper','collectionRapper','text!templates/page-rappeur.html','d3'], function (backbone, modelRapper,collectionRapper,jstemplate,d3) {
 
 /*==========================================================================================*/
 /*----------------------------  VUE POUR COLLECTION DE RAPPERS  ----------------------------*/
@@ -10,9 +10,10 @@ define(['backbone','modelRapper','collectionRapper','text!templates/page-rappeur
       this.render();
     },
     render: function(cp){
+      console.log($("#conteiner-module-albums"));
       var html = [];
       var that = this;
-      var d3 = this.d3;
+      //var d3 = this.d3;
       var filter = this.filter;
       this.collection.fetch({
         success: function(rappers){
@@ -23,7 +24,7 @@ define(['backbone','modelRapper','collectionRapper','text!templates/page-rappeur
           }else{
             var rappersFiltered = rappers.where(filter);
             _.each(rappersFiltered, function(rapper){
-              d3(rapper.toJSON());/*------ICI----------*/
+              //d3(rapper.toJSON());/*------ICI----------*/
               html.push(rapper.toJSON());
             });
           }
@@ -38,13 +39,153 @@ define(['backbone','modelRapper','collectionRapper','text!templates/page-rappeur
       })
     },
     events: {
-      "click": "affichedep"  
+      "click": "affichedep",
+      "click #album-vendu" : "d3",
+      "click #popularite" : "popularite"
     },
     affichedep : function(){
       
     },
-    d3 : function(donnee){
-      console.log(donnee);/*------ICI----------*/
+    d3 : function(e){
+      $('#conteiner-module-albums').attr('class','');
+      $("#conteiner-popularite").attr('class','displayNonePage');
+      $('#album-vendu').attr('class','activeLiFiche');
+      $('#popularite').attr('class','');
+      var idRapper = $(e.target).data('id');
+      idRapper=idRapper-1;
+      var height = 280,
+          width = 960;
+
+      var svgAlbums = d3.select(this.el).select('#graph-albums');
+
+      var pathArea;
+      var pathLine;
+      var gAlbum;
+
+      var y = d3.scale.linear()
+        .range([height, 0]);
+
+      var x = d3.scale.ordinal()
+        .rangeRoundBands([0, width], .1);
+
+      var yAxisAlbum = d3.svg.axis()
+        .scale(y)
+        .orient("left");
+
+      var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+      d3.json(this.collection.url, function(error, data) {
+          x.domain(data[idRapper].album.map(function(d,i) { return "ALBUM "+(i+1); }));
+          y.domain([0, 1000000]);
+          var nbAlbum = parseInt(data[idRapper].album.length);
+          var posAlbum = width/(nbAlbum-1);
+
+          svgAlbums.data(data[idRapper].album).enter();
+
+          svgAlbums.append("g")
+            .attr('class', 'g-axis-albums')
+            .attr('transform', "translate(-10, 0)")
+            .append("g")
+            .attr("class", "y-album axis-album")
+            .call(yAxisAlbum);
+
+          svgAlbums.append("g")
+            .attr('class', 'g-axis-albums-x')
+            .append("g")
+            .attr("class", "x-album axis-x-album")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+          d3.select(".axis-x-album").selectAll(".tick")
+            .each(function(d,i) {
+                $(this).attr("transform","translate("+(posAlbum*i)+",0)");
+            });
+
+          d3.select(".axis-x-album").select(".tick").select("text")
+            .style("text-anchor","start");
+
+          pathArea = svgAlbums.append("path").attr('class','area-album');
+          pathLine = svgAlbums.append("g").attr('class','g-line').append("path");
+          gAlbum = svgAlbums.append("g").attr('class', 'g-album');
+
+          var line = d3.svg.line()
+            .x(function(d,i) { return (posAlbum*i); })
+            .y(function(d) { return y(d.a_sells); });
+
+          pathLine.datum(data[idRapper].album)
+            .attr("class", "line")
+            .attr("d", line);
+
+          var area = d3.svg.area()
+            .x(function(d,i) { return (posAlbum*i); })
+            .y0(height)
+            .y1(function(d) { return y(d.a_sells); });
+
+          pathArea.datum(data[idRapper].album)
+            .attr("class", "area")
+            .attr("d", area);
+          
+          var barAlbums = gAlbum.selectAll("g")
+            .data(data[idRapper].album).enter()
+            .append("g")
+            .attr("class", "g-rapper")
+            .attr("transform", function(d,i){return "translate("+((posAlbum*i))+", 0)";});
+            
+          barAlbums.append("circle")
+              .attr("r",6)
+              .attr("fill","#e94953")
+              .attr("stroke","#f0f0f0") 
+              .attr("stroke-width","1")
+              .attr("cy", function(d) { return y(d.a_sells); });
+
+          barAlbums.append("circle")
+              .attr("r",6)
+              .attr("fill","#e94953")
+              .attr("stroke","#f0f0f0") 
+              .attr("stroke-width","1")
+              .attr("cy", function(d) { return y(d.a_sells); })
+              .on("mouseover", function(d,i){
+                d3.selectAll("#album-text"+i).attr("class", "activeDisplay");
+                d3.select("#album-rect"+i).attr("class", "activeDisplay");
+              })
+              .on("mouseleave", function(d,i){
+                d3.selectAll("#album-text"+i).attr("class", "");
+                d3.select("#album-rect"+i).attr("class", "");
+              });
+
+          barAlbums.append("rect")
+              .attr("id",function(d,i){return "album-rect"+i;})
+              .attr("width", "170px")
+              .attr("height", "50px")
+              .attr("rx","3")
+              .attr("ry","3")
+              .attr("fill", "#e94953")
+              .attr("x","-85")
+              .attr("y", function(d) { return y(d.a_sells)-62; });
+
+          barAlbums.append("text")
+              .style("font-size","14px")
+              .style("font-family","source_sans_prolight")
+              .attr("id",function(d,i){return "album-text"+i;})
+              .attr("y",function(d) { return y(d.a_sells)-40; })
+              .text(function(d) { return d.a_name; });
+
+          barAlbums.append("text")
+              .style("font-size","14px")
+              .style("font-family","source_sans_probold")
+              .attr("id",function(d,i){return "album-text"+i;})
+              .attr("y",function(d) { return y(d.a_sells)-20; })
+              .text(function(d) { return d.a_sells; });
+
+      });
+    },
+    popularite : function(e){
+      $('#conteiner-module-albums').attr('class','displayNonePage');
+      $("#conteiner-popularite").attr('class','');
+      $('#album-vendu').attr('class','');
+      $('#popularite').attr('class','activeLiFiche');
     }
   });
   return RapperPage;
